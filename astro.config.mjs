d8fb73999386fd8e04f9a5d386cd514a71394756
David <86719656+ScapularSteam@users.noreply.github.com>
2026-02-20 21:15:13 +0000

    created folder structure and started work on dynamic page generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()],
   },
